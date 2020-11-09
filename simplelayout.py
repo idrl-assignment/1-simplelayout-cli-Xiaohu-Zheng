f48c698c045499de3fbc0c3115eeda0176d3bc11
xiaohuzheng <zhengboy320@163.com>
2020-11-09 12:46:02 +0000

    m

--- simplelayout.py
+++ simplelayout.py
@@ -12,21 +12,21 @@ def main():
     parser.add_argument("--outdir", type=str)
     parser.add_argument("--file_name", type=str)
     args = parser.parse_args()
-    if args.board_grid % args.unit_grid != 0:
-        sys.exit()
-    max_value = (args.board_grid / args.unit_grid)**2
-    if len(args.positions) == args.unit_n:
-        for elem in args.positions:
-            if elem < 1 | elem > max_value:
-                sys.exit()
-    else:
-        sys.exit()
-    if not os.path.exists(args.outdir):
-        os.makedirs(args.outdir)
-    with open(args.outdir + "/" + args.file_name + ".png", "w") as fig:
-        pass
-    with open(args.outdir + "/" + args.file_name + ".mat", "w") as figdata:
-        pass
+#     if args.board_grid % args.unit_grid != 0:
+#         sys.exit()
+#     max_value = (args.board_grid / args.unit_grid)**2
+#     if len(args.positions) == args.unit_n:
+#         for elem in args.positions:
+#             if elem < 1 | elem > max_value:
+#                 sys.exit()
+#     else:
+#         sys.exit()
+#     if not os.path.exists(args.outdir):
+#         os.makedirs(args.outdir)
+#     with open(args.outdir + "/" + args.file_name + ".png", "w") as fig:
+#         pass
+#     with open(args.outdir + "/" + args.file_name + ".mat", "w") as figdata:
+#         pass
 
 
 if __name__ == "__main__":
