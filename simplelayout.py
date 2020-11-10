e296e4e46f9995d63108ecd2d5f5daca88d1fc78
xiaohuzheng <zhengboy320@163.com>
2020-11-10 02:35:05 +0000

    提交作业

--- simplelayout.py
+++ simplelayout.py
@@ -5,17 +5,17 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--board_grid", type=int)
-    parser.add_argument("--unit_grid", type=int)
-    parser.add_argument("--unit_n", type=int)
-    parser.add_argument("--positions", type=int, nargs="+")
-    parser.add_argument("--outdir", type=str)
-    parser.add_argument("--file_name", type=str)
+    parser.add_argument("--board_grid", type=int)
+    parser.add_argument("--unit_grid", type=int)
+    parser.add_argument("--unit_n", type=int)
+    parser.add_argument("--positions", type=int, nargs="+")
+    parser.add_argument("--outdir", type=str)
+    parser.add_argument("--file_name", type=str)
     args = parser.parse_args()
 
     if args.board_grid % args.unit_grid != 0:
         sys.exit()
-    
+
     upper_value = (args.board_grid / args.unit_grid)**2
     if len(args.positions) == args.unit_n:
         if min(args.positions) < 1 or max(args.positions) > upper_value:
@@ -25,10 +25,10 @@ def main():
 
     if not os.path.exists(args.outdir):
         os.makedirs(args.o)
-    
-    with open(args.outdir + "/" + args.file_name + ".png", "w") as fig:
+
+    with open(args.outdir + "/" + args.file_name + ".png", "w") as fig:
         pass
-    with open(args.outdir + "/" + args.file_name + ".mat", "w") as figdata:
+    with open(args.outdir + "/" + args.file_name + ".mat", "w") as figdata:
         pass
 
 
